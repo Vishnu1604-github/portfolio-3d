586cb8c997436f68118bc318e2dbfe8910417e4d
Vishnu1604-github <youremail@example.com>
2025-08-12 22:01:16 -0500

    Fix case sensitivity issue for Education component

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import About from "./About";
 import Tech from "./Tech";
 import Experience from "./Experience";
 import Works from "./Works";
-import Education from "./education";
+import Education from "./Education";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
 
